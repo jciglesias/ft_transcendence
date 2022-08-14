16ab7f713fcc77e9a892948d6b1c5d997a0c9e8d
Juan Carlos <jiglesia@student.42.fr>
2022-08-14 14:42:28 +0200

    multiple modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PongController } from './pong/pong.controller'
-import { PongService } from './pong/pong.service'
+// import { PongController } from './pong/pong.controller'
+import { PongModule } from './pong/pong.module';
+// import { PongService } from './pong/pong.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController, PongController],
-  providers: [AppService, PongService],
+  imports: [PongModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
